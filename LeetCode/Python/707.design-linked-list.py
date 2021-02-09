229a074d84af1bc000455f151cac8cdb78fc25db
dtr-beast <55450440+dtr-beast@users.noreply.github.com>
2021-02-09 19:21:45 +0530

    09022021

--- LeetCode/Python/707.design-linked-list.py
+++ LeetCode/Python/707.design-linked-list.py
@@ -1,3 +1,8 @@
+# @before-stub-for-debug-begin
+# from python3problem707 import *
+from typing import *
+# @before-stub-for-debug-end
+
 #
 # @lc app=leetcode id=707 lang=python3
 #
@@ -5,44 +10,100 @@
 #
 
 # @lc code=start
-# TODO: Implement Single and Doubly linked list (or only doubly linked list)
-class MyLinkedList:
+class Node:
+    def __init__(self, val=0) -> None:
+        self.val = val
+        self.next: Node = None
+        self.prev: Node = None
+
+
+class MyLinkedList(object):
 
     def __init__(self):
         """
         Initialize your data structure here.
         """
-        
+        self.head: Node = None
+        self.size: int = 0
 
-    def get(self, index: int) -> int:
+    def get(self, index) -> int:
         """
         Get the value of the index-th node in the linked list. If the index is invalid, return -1.
         """
-        
+        if index < 0 or index >= self.size:
+            return -1
 
-    def addAtHead(self, val: int) -> None:
-        """
-        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
-        """
-        
+        if self.head is None:
+            return -1
 
-    def addAtTail(self, val: int) -> None:
+        currNode = self.head
+        for _ in range(index):
+            currNode = currNode.next
+        
+        return currNode.val
+
+    def addAtHead(self, val) -> None:
+        """
+        Add a node of value val before the first element of the linked list.
+        After the insertion, the new node will be the first node of the linked list.
+        """
+        newHead = Node(val)
+        newHead.next = self.head
+        self.head = newHead
+
+        self.size += 1
+
+    def addAtTail(self, val) -> None:
         """
         Append a node of value val to the last element of the linked list.
         """
-        
+        curr = self.head
+        if curr is None:
+            self.head = Node(val)
+        else:
+            while curr.next is not None:
+                curr = curr.next
+            curr.next = Node(val)
 
-    def addAtIndex(self, index: int, val: int) -> None:
-        """
-        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
-        """
-        
+        self.size += 1
 
-    def deleteAtIndex(self, index: int) -> None:
+    def addAtIndex(self, index, val) -> None:
+        """
+        Add a node of value val before the index-th node in the linked list.
+        If index equals to the length of linked list, the node will be appended to the end of linked list.
+        If index is greater than the length, the node will not be inserted.
+        """
+        if index < 0 or index > self.size:
+            return
+
+        if index == 0:
+            self.addAtHead(val)
+        else:
+            curr = self.head
+            for _ in range(index - 1):
+                curr = curr.next
+            newNode = Node(val)
+            newNode.next = curr.next
+            curr.next = newNode
+
+            self.size += 1
+
+    def deleteAtIndex(self, index) -> None:
         """
         Delete the index-th node in the linked list, if the index is valid.
         """
-        
+        if index < 0 or index >= self.size:
+            return
+
+        curr = self.head
+        if index == 0:
+            self.head = curr.next
+        else:
+            for _ in range(index - 1):
+                curr = curr.next
+            curr.next = curr.next.next
+
+        self.size -= 1
 
 
 # Your MyLinkedList object will be instantiated and called as such:
@@ -53,4 +114,3 @@ class MyLinkedList:
 # obj.addAtIndex(index,val)
 # obj.deleteAtIndex(index)
 # @lc code=end
-
