f975db940dd03f53825fbed80e732f40a3fd6c9c
Aditya Sharma <mc5torkpro@gmail.com>
2024-12-05 23:09:27 +0530

    Update 21. Merge two sorted lists

--- LeetCode/Python/21.merge-two-sorted-lists.py
+++ LeetCode/Python/21.merge-two-sorted-lists.py
@@ -1,8 +1,7 @@
-# @before-stub-for-debug-begin
-from python3problem21 import *
-from typing import *
-# @before-stub-for-debug-end
-
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 #
 # @lc app=leetcode id=21 lang=python3
 #
@@ -17,25 +16,21 @@ from typing import *
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        # Dummy Node for edge cases (Null Nodes)
-        dummyNode = lst = ListNode(0)
+        head = ListNode()
 
+        temp = head
         while l1 and l2:
             if l1.val < l2.val:
-                lst.next = l1
+                temp.next = l1
                 l1 = l1.next
-            
             else:
-                lst.next = l2
-                l2 = l2.next
+                temp.next = l2
+                l2 = l2.next 
             
-            lst = lst.next
-        
-        # Since we know the above while loop ended because of any one node becoming empty
-        # We can directly assign the other node to the list
-        lst.next = l1 or l2
-     
-        return dummyNode.next
-        
-# @lc code=end
+            temp = temp.next
 
+        temp.next = l1 or l2
+        return head.next
+
+
+# @lc code=end
