1c1a0c42c58a911f334b2e4d9effa24b1fd66236
Aditya Sharma <mc5torkpro@gmail.com>
2024-12-07 18:29:02 +0530

    Redo [19] Remove Nth Node From End of List

--- LeetCode/Python/19.remove-nth-node-from-end-of-list.py
+++ LeetCode/Python/19.remove-nth-node-from-end-of-list.py
@@ -3,6 +3,14 @@
 #
 # [19] Remove Nth Node From End of List
 #
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 
 # @lc code=start
 # Definition for singly-linked list.
@@ -10,50 +18,24 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-# TODO: 190/208
 class Solution:
-    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        dummyNode = ListNode()
-        dummyNode.next = head
-
-        leader = follower = dummyNode
-
-        # Maintain a gap of 'n', so the leader node notifies ending of the list of nodes
-        for _ in range(n + 1):
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # Set the follower 1 node behind so that it is exactly one behind the node to remove  
+        dummy = follower = ListNode()
+        dummy.next = head
+        leader = head
+        
+        while n > 0:
+            n -= 1
             leader = leader.next
 
-        while leader:
+        while leader is not None:
             leader = leader.next
             follower = follower.next
 
         follower.next = follower.next.next
 
-        # Returning dummyNode.next instead of head guarantees dealing with corner cases
-        # len(ListNode) < 3
-        # Since the changes will be accommodated in the dummyNode.
-        return dummyNode.next
+        return dummy.next
 
-        """
-        My Solution (above one copied from LeetCode):
-        Could not handle Corner cases, where the length of the Linked List is smaller than 3
-        nodes = []
-        maxNodeLen = n + 1
-        tempHead = head
 
-        while tempHead:
-            nodes.append(tempHead)
-            if len(nodes) > maxNodeLen:
-                nodes.pop(0)
-            tempHead = tempHead.next
-
-        if len(nodes) == 1:
-            return None
-
-        try:
-            nodes[len(nodes) - n].next = nodes[len(nodes) - n].next.next
-            del nodes[len(nodes) - n + 1]
-        except IndexError:
-            pass
-            # nodes[0].next = None
-        """
 # @lc code=end
