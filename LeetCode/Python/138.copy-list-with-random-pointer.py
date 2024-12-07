eea0ea6bd501bc663ebb11614ce960c398ce86c0
Aditya Sharma <mc5torkpro@gmail.com>
2024-12-07 20:37:13 +0530

    Redo [138] Copy List with Random Pointer

--- LeetCode/Python/138.copy-list-with-random-pointer.py
+++ LeetCode/Python/138.copy-list-with-random-pointer.py
@@ -1,65 +1,57 @@
-# @before-stub-for-debug-begin
-from python3problem138 import *
-from typing import *
-# @before-stub-for-debug-end
-
 #
 # @lc app=leetcode id=138 lang=python3
 #
 # [138] Copy List with Random Pointer
 #
-# TODO: Improve
+
+from typing import Optional
+
+
+class Node:
+    def __init__(self, x: int, next: "Node" = None, random: "Node" = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+
 # @lc code=start
-# """
+"""
 # Definition for a Node.
 class Node:
     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
         self.val = int(x)
         self.next = next
         self.random = random
-# """
+"""
 
+
+# TODO: Try again yourself
 class Solution:
-    def copyRandomList(self, head: 'Node') -> 'Node':
-        if head is None:
-            return
+    nodeDict = {}
 
-        new_head = temp_new_head = Node(-1)
-        temp_head = head
-        old_node_list = []
-        new_node_list = []
-        
-        while temp_head:
-            temp_new_head.next = Node(temp_head.val)
+    def copyRandomList(self, head: "Optional[Node]") -> "Optional[Node]":
+        deepCopyDummy = deepTemp = Node(0)
+        temp = head
 
-            old_node_list.append(temp_head)
-            temp_head = temp_head.next
-            
-            temp_new_head = temp_new_head.next
-            new_node_list.append(temp_new_head)
-            
-        
-        temp_head = head
-        lst_len = len(new_node_list)
+        while temp is not None:
+            n = self.getDefault(temp, temp.val)
 
-        for i in range(lst_len):
-            if old_node_list[i].random:
-                for j in range(lst_len):
-                    if old_node_list[i].random is old_node_list[j]:
-                        new_node_list[i].random = new_node_list[j]
-                        break
-        
-        return new_head.next
+            if temp.random is not None:
+                rn = self.getDefault(temp.random, temp.random.val)
+                n.random = rn
 
-    
-    def print_list(self, head: 'Node'):
-        while head:
-            print(head.val)
-            head = head.next
-    
-    def node_list_val(self, lst: List[Node]):
-        for i in lst:
-            print(i.val, end=" ")
-        
-        print()
+            deepTemp.next = n
+            deepTemp = deepTemp.next
+            temp = temp.next
+
+        return deepCopyDummy.next
+
+    def getDefault(self, val: "Node", value=0):
+        if val not in self.nodeDict:
+            n = Node(value)
+            self.nodeDict[val] = n
+
+        return self.nodeDict[val]
+
+
 # @lc code=end
